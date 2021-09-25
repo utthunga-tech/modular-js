d1837a764857af77dab050f80178c8eb62f8592a
Venugopal  K (C) <vgk@tritechsamvit.com>
2021-09-25 08:09:15 +0530

    Test Suite Added

---
+++ spec/calculator.spec.js
@@ -0,0 +1,9 @@
+const add=require('../calculator.js')
+describe("Calculator Test Suite ", function() {
+    describe("Add function test", function() {
+        it("Assert True", function() {
+          const result=add(10,20);
+          expect(result).toBe(30);
+        });
+      });
+});
